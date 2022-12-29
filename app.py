dfc433047b020ee5d69b8064ca3c55beee14b847
RulSam <114384211+RulSam@users.noreply.github.com>
2022-12-29 16:52:25 +0500

    instruction added

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from extensions import APIException, Convertor
 from config import TOKEN, exchanges
 import traceback
 
-/
+
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message: telebot.types.Message):
-    text = "Приветствие!"
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты>\nУвидить список всех доступных валют: /values'
     bot.send_message(message.chat.id, text)
 
 
